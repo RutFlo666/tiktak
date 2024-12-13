a6a205d684cd287c65e66d634759098fcf6335f9
opilane <opilane@HP-CZC0359NN9>
2024-12-13 10:18:42 +0200

    Add project files.

---
+++ tiktak/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace tiktak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha üks if ja else nestimine iseseisvalt");
+            Console.WriteLine("Else if-i sisse panna if ja else võrdlus");
+            Console.WriteLine("Sisesta number:");
+
+            string input = Console.ReadLine(); 
+
+            if (int.TryParse(input, out int number)) 
+            {
+                if (number > 0) 
+                {
+                    Console.WriteLine("Number on positiivne.");
+                }
+                else 
+                {
+                    Console.WriteLine("Number ei ole positiivne.");
+                }
+
+               
+                if (number % 2 == 0) 
+                {
+                    Console.WriteLine("Number on paaris.");
+                }
+                else 
+                {
+                    Console.WriteLine("Number on paaritu.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Palun sisesta korrektne täisarv!"); 
+            }
+        }
+    }
+}
